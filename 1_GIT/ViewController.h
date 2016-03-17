e04e9076beea6871d16cfa21540d75890b5c1665
vivekvv <vivek@Vivek-Karuppanarajs-iMac.local>
2016-03-17 18:34:01 +0530

    commit 1

---
+++ 1_GIT/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  1_GIT
+//
+//  Created by Vivek Karuppanaraj on 17/03/16.
+//  Copyright © 2016 Vivek Karuppanaraj. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ 1_GIT/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  1_GIT
+//
+//  Created by Vivek Karuppanaraj on 17/03/16.
+//  Copyright © 2016 Vivek Karuppanaraj. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
